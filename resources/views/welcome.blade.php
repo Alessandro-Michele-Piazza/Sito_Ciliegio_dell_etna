19f9986a2a7e5da18741b5161b4266e3bb24a5e6
Alessandro Michele Piazza <alexpiazza98@gmail.com>
2026-01-17 20:10:04 +0100

    completata prima section sotto il video

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,9 +6,48 @@
             <source src="{{ asset('storage/media/video_ciliegio_home.mp4') }}" type="video/mp4">
         </video>
     </div>
-    <div class="padding-prova">
 
-    </div>
+    <section class="container section-mission py-5">
+        <div class="row align-items-start">
 
+            <div class="col-lg-5 col-md-12 mb-5 mb-lg-0">
+                <span class="sub-label">DAL 2005</span>
+                <h2 class="titolo-mission">
+                    Il Ciliegio dell'Etna <br>
+                    <span class="testo-radici-sul-vulcano">Radici sul Vulcano</span>
+                </h2>
+                <div class="divider-short"></div>
+            </div>
+
+            <div class="col-lg-7 col-md-12">
+                <div class="content-wrapper">
+                    <p class="descrizione-mission">
+                        Il nostro <strong class="scritta_verde_medio">agriturismo a Giarre</strong> racconta una storia
+                        di passione
+                        legata
+                        indissolubilmente al territorio dell’<strong class="scritta_verde_medio"> Etna</strong>. La
+                        nostra missione è portarvi alla
+                        scoperta della vera <strong class="scritta_verde_medio">cucina siciliana</strong>, fatta di
+                        gesti antichi e ingredienti
+                        purissimi.
+                    </p>
+                    <p class="descrizione-mission mt-4">
+                        Nel nostro <strong class="scritta_verde_medio">ristorante</strong> e nella nostra <strong
+                            class="scritta_verde_medio">pizzeria</strong>, i
+                        <strong class="scritta_verde_medio">prodotti tipici</strong> a chilometro zero diventano i
+                        protagonisti: dalle verdure del
+                        nostro orto alle farine pregiate. Siamo la cornice ideale per celebrare i vostri
+                        <strong class="scritta_verde_medio">banchetti</strong>, avvolti dal fascino unico della terra
+                        vulcanica.
+                    </p>
+                    <a href="#" class="link-scopri mt-4">
+                        <span>Contattaci</span>
+                        <i class="fa-solid fa-arrow-right-long ms-2"></i>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    </section>
 
 </x-layout>
