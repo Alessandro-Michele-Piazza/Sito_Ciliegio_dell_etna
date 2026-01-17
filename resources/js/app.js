e2675bc17640b0de6fd394978d2c6c19644834b3
Alessandro Michele Piazza <alexpiazza98@gmail.com>
2026-01-17 15:58:04 +0100

    completata navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,19 @@
-import './bootstrap';
-import 'bootstrap';
+import './bootstrap'; // Se usi Laravel
+import 'bootstrap';    // Importa il JS di bootstrap
+
+const navbar = document.querySelector(".navbar");
+const collapseElement = document.getElementById('navbarNav');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) { // Meglio un valore > 0 per evitare micro-scroll
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+        
+        // Se il menu era aperto, lo chiudiamo quando torniamo in cima (opzionale)
+        if(collapseElement.classList.contains('show')) {
+             // Usa l'API di Bootstrap per chiuderlo correttamente se possibile
+             collapseElement.classList.remove('show');
+        }
+    }
+});
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,40 +1,51 @@
 <nav class="navbar navbar-expand-lg" id="navbar">
     <div class="container-fluid">
-        <div class="d-flex align-items-center">
-            <button class="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                id="hamburgerBtn">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">
-                <img src="{{ Vite::asset('resources/images/logo_ciliegio.png') }}" alt="Logo" class="icona_navbar">
-            </a>
-        </div>
+        <div class="d-flex justify-content-between w-100 align-items-center">
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Ristorante</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Pizzeria</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Hotel</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle fs-5" href="#" role="button"
-                        data-bs-toggle="dropdown">Servizi</a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Corsi di cucina</a></li>
-                        <li><a class="dropdown-item" href="#">Corsi di pizzeria</a></li>
-                        <li><a class="dropdown-item" href="#">Corso di Panificazione</a></li>
-                        <li><a class="dropdown-item" href="#">Wine Tour</a></li>
+            <div class="d-flex align-items-center">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ Vite::asset('resources/images/logo_ciliegio.png') }}" alt="Logo" class="icona_navbar">
+                </a>
+            </div>
+
+            <div>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Ristorante</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Pizzeria</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Hotel</a></li>
+
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle fs-5" href="#" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Servizi
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Corsi di cucina</a></li>
+                                <li><a class="dropdown-item" href="#">Corsi di pizzeria</a></li>
+                                <li><a class="dropdown-item" href="#">Corso di Panificazione</a></li>
+                                <li><a class="dropdown-item" href="#">Wine Tour</a></li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Prodotti Tipici</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Esperienze</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Blog</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Menù Domenicale</a></li>
+                        <li class="nav-item"><a class="nav-link fs-5" href="#">Contatti</a></li>
                     </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Prodotti Tipici</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Esperienze</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Blog</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Menù Domenicale</a></li>
-                <li class="nav-item"><a class="nav-link fs-5" href="#">Contatti</a></li>
-            </ul>
-        </div>
+                </div>
+            </div>
 
-        <div class="d-flex align-items-center">
-            <a class="btn btn-prenota" href="#">Prenota Ora</a>
+            <div class="d-flex align-items-center">
+                <a class="btn btn-prenota" href="#">Prenota Ora</a>
+                <button class="navbar-toggler me-2 mx-3" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                    aria-label="Toggle navigation" id="hamburgerBtn">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
         </div>
     </div>
 </nav>
