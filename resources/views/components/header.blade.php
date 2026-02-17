7f4474860baec14d81c603e552d3c8a7a1694f06
Alessandro Michele Piazza <alexpiazza98@gmail.com>
2026-02-17 12:35:38 +0100

    testing

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,7 @@
 <section class="header header-modern d-flex align-items-center justify-content-center">
     <div class="header-overlay"></div>
     <div class="container text-center position-relative" data-aos="fade-up">
-        <span class="sub-label header-badge">IL CILIEGIO DELL'ETNA.</span>
+        <span class="sub-label header-badge">IL CILIEGIO DELL'ETNA</span>
         <h1 class="display-4 text-white mt-3">{{ html_entity_decode($title ?? '') }}</h1>
         <p class="header-subtitle text-white mb-0">{{ __('ui.header_subtitle') }}</p>
     </div>
